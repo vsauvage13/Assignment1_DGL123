990b031fb25bf051d1b1fc852ff56cb2f3e90428
vsauvage13 <90713179+vsauvage13@users.noreply.github.com>
2023-09-11 19:46:09 -0700

    Added Item to Array
    Adds a new value to the place of 0 index to show my favourite candy.

--- echo-shorthand.php
+++ echo-shorthand.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Vienna May';
+$favorites = ['Chocolate Covered Almonds', 'Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
