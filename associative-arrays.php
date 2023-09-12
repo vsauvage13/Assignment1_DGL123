ff961e56ce6cc4dfc30d380c01eca8c281f99b25
vsauvage13 <90713179+vsauvage13@users.noreply.github.com>
2023-09-11 19:26:46 -0700

    Added Element
    Added Element with protein key and different value to the array.

--- associative-arrays.php
+++ associative-arrays.php
@@ -1,8 +1,9 @@
 <?php 
 $nutrition = [
-    'fat'   => 16,
-    'sugar' => 51,
-    'salt'  => 6.3,
+    'fat'   => 42,
+    'sugar' => 60,
+    'salt'  => 3.5,
+    'protein' => 2.6,
 ];
 ?>
 <!DOCTYPE html>
@@ -17,5 +18,6 @@ $nutrition = [
     <p>Fat:   <?php echo $nutrition['fat']; ?>%</p>
     <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
     <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
+    <p>Protein:  <?php echo $nutrition['protein']; ?>%</p>
   </body>
 </html>
